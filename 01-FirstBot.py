e0429e7913153aa5b22e4f03a086c889ba64d4bf
LarissaMikolaschek <larissa.mikolaschek@t-online.de>
2020-11-25 21:54:33 +0100

    initial commit

---
+++ 01-FirstBot.py
@@ -0,0 +1,26 @@
+import time
+import random
+
+
+name = input("Hello, what is your name? ")
+
+time.sleep(2.0)
+
+print("Hello " + name)
+
+feeling = input("How are you today? ")
+
+time.sleep(2.0)
+
+if 'good' in feeling:
+    print("I'm feeling good too!")
+else:
+    print("I'm sorry to hear that!")
+
+time.sleep(2.0)
+favcolour = input("What is your favourite colour? ")
+
+colours = ["Red", "Green", "Blue"]
+
+time.sleep(2)
+print("My favourite colour is " + random.choice(colours))
---
+++ README.md
@@ -0,0 +1,4 @@
+## Baue deinen eigenen Chatbot!
+
+Aleksa, Siri &Co. : Chatbots sind überall. Doch wie schaffen sie es, uns die passenden Antworten auf unsere Fragen zu geben? Und wie lassen sie sich austricksen?
+Wir gehen diesen Fragen auf den Grund, indem wir unseren eigenen Chatbot von Grund auf selber bauen. Eurer Kreativität sind dabei keine Grenzen gesetzt!
